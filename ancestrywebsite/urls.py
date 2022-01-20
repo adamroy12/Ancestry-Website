630a9a1a3672ee03cec638d68a146f1ed4beb567
Adam Royal <adamroyal12@gmail.com>
2022-01-20 21:54:32 +0000

    log in/log out feature added

--- ancestrywebsite/urls.py
+++ ancestrywebsite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
 
 from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,9 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('blog', views.blog, name='blog'),
     path('contact', views.contact, name='contact'),
-]
+
+
+    #Django Auth
+    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
+    ]
