c51f322970a583eca5f0c176be73b0ab459a7922
Adam Royal <adamroyal12@gmail.com>
2022-01-21 16:58:19 +0000

    added some database models and migrated. Implemented phonenumber/email fields also

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 "# ancestry-website" 
 An attempt at a website for my dad's genealogy stuff.
+
+
+Using Stefan Foulis' django phonenumber field: https://github.com/stefanfoulis/django-phonenumber-field
--- ancestrywebsite/apps/accounts/models.py
+++ ancestrywebsite/apps/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 class UserPersona(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -19,5 +20,9 @@ class UserProfile(models.Model):
     #Ancestry Information
     info = models.CharField(max_length=1000, blank=True, null=True)
     persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
+
+    #contact details
+    emailaddress = models.EmailField(max_length=250, default="default_email")
+    phone_number = PhoneNumberField(blank=True)
 
 
